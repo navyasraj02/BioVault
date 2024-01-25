c52c8b9fa495c14f754586c448831ba27961ab32
navyasraj02 <navyasraj2003@gmail.com>
2024-01-25 17:13:13 +0530

    added PyMongo library

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
-app=Flask(__name__)
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://donajohn31:Prgo1@band@cluster.kebfxpl.mongodb.net/?retryWrites=true&w=majority"
+mongo = PyMongo(app)
 
 @app.route("/")
 def hello_world():
     return "<p> Hello World</p>"
+
+app.run(debug=True)
