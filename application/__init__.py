716fc259b5296303d08e7c0e2bce0c5bafb4395c
navyasraj02 <navyasraj2003@gmail.com>
2024-01-29 21:26:17 +0530

    modified routes.py

--- application/__init__.py
+++ application/__init__.py
@@ -3,10 +3,10 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "db24c608640f5034b30b8e1e1eb5618ed0ffdbf5"
-# app.config["MONGO_URI"] = "mongodb+srv://donajohn31:Prgo1@band@cluster.kebfxpl.mongodb.net/?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://donajohn31:Progband@cluster1.u0j3wol.mongodb.net/biovault?retryWrites=true&w=majority"
 
 # mongodb database
-# mongodb_client = PyMongo(app)
-# db = mongodb_client.db
+mongodb_client = PyMongo(app)
+db = mongodb_client.db
 
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -4,15 +4,27 @@ from flask import render_template, request, redirect, flash, url_for
 from bson import ObjectId
 
 from .forms import UserData
-# from application import db
-# from datetime import datatime
+from application import db
 
 @app.route("/")
 def index():
     return "<p> Hello World!</p>"
 
 
-@app.route("/get_user_data")
+@app.route("/get_user_data", methods=["POST","GET"])
 def get_user_data():
-    form = UserData()
+    if request.method== "POST":
+        form = UserData(request.form)
+        user_name = form.name.data
+        user_email = form.email.data
+
+        db.regUser.insert_one({
+            "name" : user_name,
+            "email" : user_email
+        })
+
+        flash("Sign Up Successful","success")
+        return redirect("/")
+    else:
+        form = UserData()
     return render_template("get_user_data.html",form = form)
