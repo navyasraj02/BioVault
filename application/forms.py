459938c18ccbee07c1a07d5c9ddf59acc43dc25c
navyasraj02 <navyasraj2003@gmail.com>
2024-01-29 20:00:38 +0530

    added HTML file

--- application/forms.py
+++ application/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import validators, StringField, TextAreaField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 
-class userData(FlaskForm):
-    name = StringField('name', [validators.DataRequired()])
-    email = StringField('email', [validators.DataRequired(), validators.Email()])
+class UserData(FlaskForm):
+    name = StringField('Name', [validators.DataRequired()])
+    email = StringField('Email', [validators.DataRequired(), validators.Email()])
     submit = SubmitField("Sign Up")
--- application/routes.py
+++ application/routes.py
@@ -3,10 +3,16 @@ from flask import render_template, request, redirect, flash, url_for
 
 from bson import ObjectId
 
-from .forms import userData
-from application import db
-from datetime import datatime
+from .forms import UserData
+# from application import db
+# from datetime import datatime
 
 @app.route("/")
+def index():
+    return "<p> Hello World!</p>"
+
+
+@app.route("/get_user_data")
 def get_user_data():
-    
+    form = UserData()
+    return render_template("get_user_data.html",form = form)
