8d8cb43f25a66e2ff1fa4ae9e553d000c02d486c
navyasraj02 <navyasraj2003@gmail.com>
2024-01-30 15:51:32 +0530

    modified routes.py

--- application/__init__.py
+++ application/__init__.py
@@ -4,6 +4,8 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "db24c608640f5034b30b8e1e1eb5618ed0ffdbf5"
 app.config["MONGO_URI"] = "mongodb+srv://donajohn31:Progband@cluster1.u0j3wol.mongodb.net/biovault?retryWrites=true&w=majority"
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+app.config['UPLOAD_FOLDER'] = 'sample'
 
 # mongodb database
 mongodb_client = PyMongo(app)
--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,8 @@
 from application import app
 from flask import render_template, request, redirect, flash, url_for
-
+import os
+from PIL import Image
+from werkzeug.utils import secure_filename
 from bson import ObjectId
 
 from .forms import UserData
@@ -15,16 +17,29 @@ def index():
 def get_user_data():
     if request.method== "POST":
         form = UserData(request.form)
-        user_name = form.name.data
-        user_email = form.email.data
 
-        db.regUser.insert_one({
-            "name" : user_name,
-            "email" : user_email
-        })
+        # print(form.errors)
+        # print(form.validate_on_submit())
 
-        flash("Sign Up Successful","success")
-        return redirect("/")
+        if 'fimg' in request.files and form.validate_on_submit():
+            print("hello")
+            user_fimg = request.files['fimg']
+            user_name = form.name.data
+            user_email = form.email.data
+        
+            filename = secure_filename(user_fimg.filename)
+            userFile = os.path.join("sample", filename)
+            s = user_fimg.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'], filename))
+            print("File saved successfully:", userFile)
+
+            db.regUser.insert_one({
+                "name" : user_name,
+                "email" : user_email
+                
+            })
+
+            flash("Sign Up Successful","success")
+            return redirect("/")
     else:
         form = UserData()
     return render_template("get_user_data.html",form = form)
