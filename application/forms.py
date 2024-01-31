a3addfb852779234155085cf29267c07340ea37c
navyasraj02 <navyasraj2003@gmail.com>
2024-01-31 14:03:30 +0530

    updated routes.py

--- application/forms.py
+++ application/forms.py
@@ -5,5 +5,5 @@ from wtforms.validators import DataRequired, InputRequired, Email
 class UserData(FlaskForm):
     name = StringField('Name', [validators.DataRequired()])
     email = StringField('Email', [validators.DataRequired(), validators.Email()])
-    fimg = FileField("Upload Fingerprint",[validators.InputRequired()] )
+    fimg = FileField("Upload Fingerprint",validators=[InputRequired()])
     submit = SubmitField("Sign Up")
--- application/routes.py
+++ application/routes.py
@@ -16,30 +16,29 @@ def index():
 @app.route("/get_user_data", methods=["POST","GET"])
 def get_user_data():
     if request.method== "POST":
-        form = UserData(request.form)
+        form = UserData()
+        if form.validate_on_submit():
+            user_fimg = request.files.get('fimg')
+            if user_fimg:
+                user_name = form.name.data
+                user_email = form.email.data
+            
+                filename = secure_filename(user_fimg.filename)
+                userFile = os.path.join("sample", filename)
+                upload_folder = app.config['UPLOAD_FOLDER']
+                s = user_fimg.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), upload_folder, filename))
+                print("File saved successfully:", userFile)
 
-        # print(form.errors)
-        # print(form.validate_on_submit())
+                db.regUser.insert_one({
+                    "name" : user_name,
+                    "email" : user_email
+                    
+                })
 
-        if 'fimg' in request.files and form.validate_on_submit():
-            print("hello")
-            user_fimg = request.files['fimg']
-            user_name = form.name.data
-            user_email = form.email.data
-        
-            filename = secure_filename(user_fimg.filename)
-            userFile = os.path.join("sample", filename)
-            s = user_fimg.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'], filename))
-            print("File saved successfully:", userFile)
-
-            db.regUser.insert_one({
-                "name" : user_name,
-                "email" : user_email
-                
-            })
-
-            flash("Sign Up Successful","success")
-            return redirect("/")
+                flash("Sign Up Successful","success")
+                return redirect("/")
+            else:
+                flash("No file uploaded", "error")
     else:
         form = UserData()
     return render_template("get_user_data.html",form = form)
