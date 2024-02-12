dc4382abbe4c98799747167e653243be5dba18b9
navyasraj02 <navyasraj2003@gmail.com>
2024-02-12 15:29:15 +0530

    updated routes.py

--- application/routes.py
+++ application/routes.py
@@ -3,7 +3,6 @@ from flask import jsonify, render_template, request, redirect, flash, url_for
 import os
 from PIL import Image
 from werkzeug.utils import secure_filename
-from bson import ObjectId
 
 from .route_func import pinGen,fpMatch  
 from .forms import UserData
@@ -15,62 +14,83 @@ sample_dir = os.path.join(parent_dir, 'sample')
 
 @app.route("/")
 def index():
-    return "<p> Hello World!</p>"
+    return  {"status": "success", "message": "Integrate Flask Framework with Next.js"}
 
+@app.route("/api/register", methods=["POST","GET"])
+def register():
+    name = request.json.get("name")
+    email = request.json.get("email")
+    fpimg = request.files['image']
 
-@app.route("/get_user_data", methods=["POST","GET"])
-def get_user_data():
-    if request.method== "POST":
-        form = UserData()
-        if form.validate_on_submit():
-            user_fimg = request.files.get('fimg')
-            if user_fimg:
-                user_name = form.name.data
-                user_email = form.email.data
-            
-                filename = secure_filename(user_fimg.filename)
-                userFile = os.path.join("sample", filename)
-                upload_folder = app.config['UPLOAD_FOLDER']
-                # user_fimg.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), upload_folder, filename))
-                # print("File saved successfully:", userFile)
+    filename = secure_filename(fpimg.filename)
+    fpimg.save(os.path.join(sample_dir, filename))
+    print("File saved successfully")
 
-                print("Sign Up Successful","success")
-                # pinGen.generate_secure_token()
-                
-                #-----TO DO: file1 will be uploaded by user, file2 retrieved from the database-----
-                # Construct the path to the file using os.path.join
-                file_path1 = os.path.join(os.path.abspath(os.path.dirname(__file__)), upload_folder, "fa1.BMP")
-                file_path2 = os.path.join(os.path.abspath(os.path.dirname(__file__)), upload_folder, "fa3.BMP")
+    #Perform encryption logic  
 
-                # Check if the file exists before attempting to open it
-                if os.path.exists(file_path1) and os.path.exists(file_path2) :
-                    score=fpMatch.fingerprint_similarity(file_path1,file_path2)
-                    print("Matching score: ",score)
+    db.regUser.insert_one({
+        "name" : name,
+        "email" : email             
+    })
 
-                """db.regUser.insert_one({
-                    "name" : user_name,
-                    "email" : user_email
-                    
-                }) """
+    # delete_files(sample_dir)
 
-                # Delete files from sample folder
-                # delete_files(sample_dir)
-                return redirect("/")
-            else:
-                flash("No file uploaded", "error")
-    else:
-        form = UserData()
-    return render_template("get_user_data.html",form = form)
+    return jsonify({"message" : "Registration successfull"})
 
-# --------DELETE FILE FUNC---------
+# --------delete file func---------
 def delete_files(folder_path):
-    # Define the folder path
     # folder_path = 'path/to/your/folder'
 
-    # Get the list of files in the folder
     file_list = os.listdir(folder_path)
 
-    # Iterate over the files and delete them
     for file_name in file_list:
         file_path = os.path.join(folder_path, file_name)
         os.remove(file_path)
+
+    # if request.method== "POST":
+    #     form = UserData()
+    #     if form.validate_on_submit():
+    #         user_fimg = request.files.get('fimg')
+    #         if user_fimg:
+    #             user_name = form.name.data
+    #             user_email = form.email.data
+            
+    #             filename = secure_filename(user_fimg.filename)
+    #             userFile = os.path.join("sample", filename)
+    #             upload_folder = app.config['UPLOAD_FOLDER']
+    #             # user_fimg.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), upload_folder, filename))
+    #             # print("File saved successfully:", userFile)
+
+    #             print("Sign Up Successful","success")
+    #             # pinGen.generate_secure_token()
+                
+    #             #-----TO DO: file1 will be uploaded by user, file2 retrieved from the database-----
+    #             # Construct the path to the file using os.path.join
+    #             file_path1 = os.path.join(os.path.abspath(os.path.dirname(__file__)), upload_folder, "fa1.BMP")
+    #             file_path2 = os.path.join(os.path.abspath(os.path.dirname(__file__)), upload_folder, "fa3.BMP")
+
+    #             # Check if the file exists before attempting to open it
+    #             if os.path.exists(file_path1) and os.path.exists(file_path2) :
+    #                 score=fpMatch.fingerprint_similarity(file_path1,file_path2)
+    #                 print("Matching score: ",score)
+
+    #             """db.regUser.insert_one({
+    #                 "name" : user_name,
+    #                 "email" : user_email
+                    
+    #             }) """
+
+    #             # Delete files from sample folder
+    #             # delete_files(sample_dir)
+    #             return redirect("/")
+    #         else:
+    #             flash("No file uploaded", "error")
+    # else:
+    #     form = UserData()
+    # return render_template("get_user_data.html",form = form)
+# MATCHING
+#     file_path1 = os.path.join(sample_dir, "fa1.BMP")    # from user
+#     file_path2 = os.path.join(sample_dir, "fa3.BMP")    # to be retrieved from DB
+#     if os.path.exists(file_path1) and os.path.exists(file_path2) :
+#         score=fpMatch.fingerprint_similarity(file_path1,file_path2)
+#         print("Matching score: ",score)
