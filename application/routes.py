8ca7d836c9606f90b9341bece2739cf0da67b85b
ms-starryvoid <donamattel@gmail.com>
2024-03-29 22:16:43 +0530

    rm keypoint json

--- application/routes.py
+++ application/routes.py
@@ -74,21 +74,22 @@ def register():
         keypoint_1 = kp_s[0]
         descrip_1 = desc[0]
         user_id_1= t_id
-        skeypoint_1 = [msgpack.dumps({
+        """skeypoint_1 = [msgpack.dumps({
         'pt': (kp.pt[0], kp.pt[1]),
         'size': kp.size,
         'angle': kp.angle,
         'response': kp.response,
         'octave': kp.octave,
         'class_id': kp.class_id
-        }) for kp in keypoint_1]
+        }) for kp in keypoint_1]"""
         # Combine data into a dictionary
         data = {
-            "keypoint": skeypoint_1.tolist(),
+            "len":len(keypoint_1),
+            #"keypoint": skeypoint_1.tolist(),
             "descrip": descrip_1.tolist(),
             "user_id": user_id_1,
         }
-        data=msgpack.dumps(data)
+        #data=msgpack.dumps(data)
         # Send POST request to receiving server
         response = requests.post(
             "https://biovault-server1.onrender.com/api/log", json={"data":data},headers={"Content-Type": "application/json"})
