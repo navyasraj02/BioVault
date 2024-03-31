a2497ef1706f246c0cbecf3d5bf82b12f654e6b9
ms-starryvoid <donamattel@gmail.com>
2024-03-31 19:32:42 +0530

    for

--- fingerprint/extract.py
+++
@@ -1,8 +0,0 @@
-import fingerprint_feature_extractor
-import cv2
-import os
-script_dir = os.path.dirname(os.path.abspath(__file__))
-image_path = os.path.join(script_dir, 'fing.jpeg')
-img = cv2.imread(image_path, 0)				# read the input image --> You can enhance the fingerprint image using the "fingerprint_enhancer" library
-FeaturesTerminations, FeaturesBifurcations = fingerprint_feature_extractor.extract_minutiae_features(img, spuriousMinutiaeThresh=10, invertImage=False, showResult=True, saveResult=True)
-print(type(FeaturesBifurcations))
